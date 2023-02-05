e8cdd29243465d9acba1fa1a34b2f49f7ce92682
tbgolfer86 <tbj86@outlook.com>
2023-02-05 11:24:16 -0500

    started index.js and added generateHTML.js

---
+++ index.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+const generateHTML = require('./src/generateHTML');
+
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
