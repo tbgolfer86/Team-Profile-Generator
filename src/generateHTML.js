20ab9215bbffae7e176676a9de411b2f65e58492
tbgolfer86 <tbj86@outlook.com>
2023-02-07 10:18:27 -0500

    added header to HTML and edited stylesheet

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -77,9 +77,13 @@ function generateHTML(data) {
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-      <title>Team Profile Generator</title>
+      <link rel="stylesheet" href="style.css">
+      <title>Team Profile</title>
   </head>
   <body>
+    <header>
+      <h1>My Team</h1>
+    </header>
     <div class="d-flex flex-wrap gap-3 justify-content-around">
      ${generateManagerCard(data)}
      ${generateEngineerCard(data)}
